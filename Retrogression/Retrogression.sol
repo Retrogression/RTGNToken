05dde7af0793a0ad5b7316fcbdd5a3352f2d146f
Retrogression <98785028+Retrogression@users.noreply.github.com>
2022-02-11 10:16:13 -0800

    Update Retrogression.sol

--- Retrogression/Retrogression.sol
+++ Retrogression/Retrogression.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.11;
+pragma solidity 0.8.11;
 //SPDX-License-Identifier: none
 
 interface IERC20 {
@@ -1049,18 +1049,23 @@ contract Retrogression is Context, ERC20, Ownable {
     bool private starting;
     bool private alreadyStarted;
     bool private stakingAllowed;
+    bool private lpAdded;
+    bool public sendTokens;
     bool public swapping;
 
 	address public productionWallet;
     address public marketingWallet;
+    address public filmCribWallet;
 
     uint256 public swapTokensAtAmount;
 
     uint256 private _buyProductionFee;
     uint256 private _buyMarketingFee;
+    uint256 private _buyFilmCribFee;
 
     uint256 private _sellProductionFee;
     uint256 private _sellMarketingFee;
+    uint256 private _sellFilmCribFee;
 
     uint256 private blacklistFee;
 
@@ -1070,10 +1075,6 @@ contract Retrogression is Context, ERC20, Ownable {
 
     uint256 public totalBuyFees;
     uint256 public totalSellFees;
-
-	uint256 public contractTokenBalanceAmount;
-
-    uint256 private constant DefaultTime = 30 days;
 
     mapping (address => bool) private _isExcludedFromFees;
 
@@ -1089,45 +1090,68 @@ contract Retrogression is Context, ERC20, Ownable {
     event tradingUpdated(bool _enabled);
     event burningUpdated(bool _enabled);
     event allowStaking(bool _enabled);
+    event sendTokensUpdated(bool _enabled);
     event SetAutomatedMarketMakerPair(address indexed pair, bool indexed value);
     event WalletUpdated(address indexed newMarketingWallet, address indexed oldMarketingWallet);
+    event UpdateSwapAmount(uint256 amount, uint256 swapTokensAtAmount);
+    event UpdateMaxWallet(uint256 newMaxWallet, uint256 _maxWallet);
+    event UpdateMaxBuySell(
+        uint256 newMaxBuy,
+        uint256 _maxBuy,
+        uint256 newMaxSell,
+        uint256 _maxSell
+    );
+    event UpdateBuyFees(
+        uint256 newBuyProductionFee,
+        uint256 _buyProductionFee,
+        uint256 newBuyMarketingFee,
+        uint256 _buyMarketingFee,
+        uint256 newBuyFilmCribFee,
+        uint256 _buyFilmCribFee
+    );
+    event UpdateSellFees(
+        uint256 newSellProductionFee,
+        uint256 _sellProductionFee,
+        uint256 newSellMarketingFee,
+        uint256 _sellMarketingFee,
+        uint256 newSellFilmCribFee,
+        uint256 _sellFilmCribFee
+    );
     event SwapAndLiquify(
         uint256 tokensSwapped,
         uint256 ethReceived,
-        uint256 tokensIntoLiqudity
-    );
-
-    event SendDividends(
-    	uint256 tokensSwapped,
-    	uint256 amount
+        uint256 tokensIntoLiquidity
     );
 
     constructor() ERC20 ("Retrogression", "RTGN") {
 
-        _buyProductionFee = 12;
+        _buyProductionFee = 11;
         _buyMarketingFee = 2;
+        _buyFilmCribFee = 1;
 
-        _sellProductionFee = 12;
+        _sellProductionFee = 11;
         _sellMarketingFee = 2;
+        _sellFilmCribFee = 1;
 
         blacklistFee = 99;
 
         alreadyStarted = false;
         stakingAllowed = false;
         trading = false;
-
-		contractTokenBalanceAmount = 2500000 * 10**18;
+        lpAdded = false;
+        sendTokens = true;
 
         swapTokensAtAmount = 2500000 * (10**18);
         _maxWallet = 5000000 * (10**18);
         _maxBuy = 2500000 * (10**18);
         _maxSell = 2500000 * (10**18);
 
-        totalBuyFees = _buyProductionFee.add(_buyMarketingFee);
-        totalSellFees = _sellProductionFee.add(_sellMarketingFee);
+        totalBuyFees = _buyProductionFee.add(_buyMarketingFee).add(_buyFilmCribFee);
+        totalSellFees = _sellProductionFee.add(_sellMarketingFee).add(_sellFilmCribFee);
 
-        productionWallet = address(payable(0x9d32eb61d265d9F19B013B549133Ea03E85Da5a1));
+        productionWallet = address(payable(0x1D80A03B5b7E13cc785547149bfa14ebF8D3Dd4C));
         marketingWallet = address(payable(0x6B1d1e793cCF87aDd7D8Ac44e0f26c2088088bBa));
+        filmCribWallet = address(payable(0xA381A36f084c5C8082a48f7F4596dC0a6FE0AC18));
 
     	IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);
     	//0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3 Testnet
@@ -1162,7 +1186,8 @@ contract Retrogression is Context, ERC20, Ownable {
 
 	function updateSwapAmount(uint256 amount) public onlyOwner {
 	    swapTokensAtAmount = amount * (10**18);
-        contractTokenBalanceAmount = amount * (10**18);
+        
+        emit UpdateSwapAmount(amount, swapTokensAtAmount);
 	}
     
     function updateUniswapV2Router(address newAddress) public onlyOwner {
@@ -1186,7 +1211,7 @@ contract Retrogression is Context, ERC20, Ownable {
     }
 
     function setAutomatedMarketMakerPair(address pair, bool value) public onlyOwner {
-        require(pair != uniswapV2Pair, "Retrogression: The PancakeSwap pair cannot be removed from automatedMarketMakerPairs");
+        require(pair != uniswapV2Pair, "Retrogression: The Uniswap pair cannot be removed from automatedMarketMakerPairs");
 
         _setAutomatedMarketMakerPair(pair, value);
     }
@@ -1212,6 +1237,13 @@ contract Retrogression is Context, ERC20, Ownable {
         productionWallet = newProductionWallet;
     }
 
+    function updateFilmCribWallet(address newFilmCribWallet) public onlyOwner {
+        require(newFilmCribWallet != filmCribWallet, "Retrogression: The film crib wallet is already this address");
+        excludeFromFees(newFilmCribWallet, true);
+        emit WalletUpdated(newFilmCribWallet, filmCribWallet);
+        filmCribWallet = newFilmCribWallet;
+    }
+
     function isExcludedFromFees(address account) public view returns(bool) {
         return _isExcludedFromFees[account];
     }
@@ -1219,6 +1251,11 @@ contract Retrogression is Context, ERC20, Ownable {
     function allowStakingAddress(bool enabled) public onlyOwner {
         stakingAllowed = enabled;
         emit allowStaking(enabled);
+    }
+
+    function SendTokensToFilmCrib(bool enabled) public onlyOwner {
+        sendTokens = enabled;
+        emit sendTokensUpdated(enabled);
     }
 
     function _transfer(
@@ -1238,9 +1275,9 @@ contract Retrogression is Context, ERC20, Ownable {
         if (from != owner() && to != owner() && to != address(0) && to != address(0xdead) && to != uniswapV2Pair && !_isExcludedFromFees[to] && !_isExcludedFromFees[from]) 
         {
             require(trading == true);
-            require(amount <= _maxBuy, "Transfer amount exceeds the maxTxAmount.");
-            uint256 contractBalanceRecepient = balanceOf(to);
-            require(contractBalanceRecepient + amount <= _maxWallet, "Exceeds maximum wallet token amount.");
+            require(amount < _maxBuy || amount == _maxBuy, "Transfer amount exceeds the maxTxAmount.");
+            uint256 contractBalanceRecipient = balanceOf(to);
+            require(contractBalanceRecipient + amount < _maxWallet || contractBalanceRecipient + amount < _maxWallet, "Exceeds maximum wallet token amount.");
         }
 
         if(amount == 0) {
@@ -1252,7 +1289,7 @@ contract Retrogression is Context, ERC20, Ownable {
         {
             require(trading == true);
 
-            require(amount <= _maxSell, "Sell transfer amount exceeds the maxSellTransactionAmount.");
+            require(amount < _maxSell || amount < _maxSell, "Sell transfer amount exceeds the maxSellTransactionAmount.");
         }
 
 		uint256 contractTokenBalance = balanceOf(address(this));
@@ -1261,18 +1298,35 @@ contract Retrogression is Context, ERC20, Ownable {
 		
 		if(canSwap && !swapping && !automatedMarketMakerPairs[from] && from != marketingWallet && to != marketingWallet && !_isExcludedFromFees[to] && !_isExcludedFromFees[from]) {
 		    
-		    contractTokenBalance = contractTokenBalanceAmount;
+		    contractTokenBalance = swapTokensAtAmount;
 		    uint256 swapTokens;
+            uint256 swapTokensAmount;
             
             swapping = true;
 
-            if (totalSellFees > 0) {
+            if (totalSellFees > 0 && !sendTokens) {
             swapTokens = contractTokenBalance;
             swapTokensForEth(swapTokens);
+            swapTokensAmount = totalSellFees;
+
+            uint256 filmCribAmount = address(this).balance.mul(_sellFilmCribFee).div(swapTokensAmount);
+            (bool success, ) = filmCribWallet.call{value: filmCribAmount}("");
+            require(success, "Failed to send film crib amount");
+            }
+
+            else if (totalSellFees > 0 && sendTokens) {
+                swapTokens = contractTokenBalance;
+                uint256 filmCribAmount = swapTokens.mul(_sellFilmCribFee).div(100);
+                swapTokens = swapTokens.sub(filmCribAmount);
+                swapTokensAmount = totalSellFees.sub(_sellFilmCribFee);
+
+                swapTokensForEth(swapTokens);
+                super._transfer(address(this), filmCribWallet, filmCribAmount);
+
             }
 
             if (_sellProductionFee > 0) {
-            uint256 productionAmount = address(this).balance.mul(_sellProductionFee).div(totalSellFees);
+            uint256 productionAmount = address(this).balance.mul(_sellProductionFee).div(swapTokensAmount);
             (bool success, ) = productionWallet.call{value: productionAmount}("");
             require(success, "Failed to send production amount");
             }
@@ -1353,35 +1407,22 @@ contract Retrogression is Context, ERC20, Ownable {
         );
     }
 
-    function addLiquidity(uint256 tokenAmount, uint256 ethAmount) private {
-
-        // approve token transfer to cover all possible scenarios
-        _approve(address(this), address(uniswapV2Router), tokenAmount);
-
-        // add the liquidity
-       uniswapV2Router.addLiquidityETH{value: ethAmount}(
-            address(this),
-            tokenAmount,
-            0, // slippage is unavoidable
-            0, // slippage is unavoidable
-            address(this),
-            block.timestamp
-        );
-    }
-
     function addLP() external onlyOwner() {
-        updateBuyFees(0,0);
-        updateSellFees(0,0);
+        require(!lpAdded, "This project is already launched");
+        updateBuyFees(0,0,0);
+        updateSellFees(0,0,0);
 
 		trading = false;
 
         updateMaxWallet(1000000000);
         updateMaxBuySell((1000000000), (1000000000));
+
+        lpAdded = true;
     }
     
 	function letsGoLive() external onlyOwner() {
-        updateBuyFees(12,2);
-        updateSellFees(12,2);
+        updateBuyFees(11,2,1);
+        updateSellFees(11,2,1);
 
         updateMaxWallet(5000000);
         updateMaxBuySell(2500000, 2500000);
@@ -1390,13 +1431,18 @@ contract Retrogression is Context, ERC20, Ownable {
         if(!trading) {
             trading = true;
         }
+        
+        if(!stakingAllowed) {
+            stakingAllowed = true;
+        }
     }
 
     function letsBegin() external onlyOwner() {
         require(!alreadyStarted, "This project is already launched");
-        _buyMarketingFee = 50;
-        _buyProductionFee = 49;
-        updateSellFees(12,2);
+        _buyMarketingFee = 33;
+        _buyProductionFee = 33;
+        _buyFilmCribFee = 33;
+        updateSellFees(11,2,1);
 
         updateMaxWallet(5000000);
         updateMaxBuySell(2500000, 2500000);
@@ -1404,27 +1450,36 @@ contract Retrogression is Context, ERC20, Ownable {
 		trading = true;
         starting = true;
         alreadyStarted = true;
+        stakingAllowed = true;
     }
 
-    function updateBuyFees(uint256 newBuyProductionFee, uint256 newBuyMarketingFee) public onlyOwner {
-        require(newBuyProductionFee.add(newBuyMarketingFee) <= 25, "Total buy taxes cannot exceed 25%");
+    function updateBuyFees(uint256 newBuyProductionFee, uint256 newBuyMarketingFee, uint256 newBuyFilmCribFee) public onlyOwner {
+        require(newBuyProductionFee.add(newBuyMarketingFee).add(newBuyFilmCribFee) <= 25, "Total buy taxes cannot exceed 25%");
         _buyProductionFee = newBuyProductionFee;
         _buyMarketingFee = newBuyMarketingFee;
+        _buyFilmCribFee = newBuyFilmCribFee;
         
         totalFees();
+
+        emit UpdateBuyFees(newBuyProductionFee, _buyProductionFee, newBuyMarketingFee, _buyMarketingFee, newBuyFilmCribFee, _buyFilmCribFee);
     }
 
-    function updateSellFees(uint256 newSellProductionFee, uint256 newSellMarketingFee) public onlyOwner {
-        require(newSellProductionFee.add(newSellMarketingFee) <= 25, "Total sell taxes cannot exceed 25%");
+    function updateSellFees(uint256 newSellProductionFee, uint256 newSellMarketingFee, uint256 newSellFilmCribFee) public onlyOwner {
+        require(newSellProductionFee.add(newSellMarketingFee).add(newSellFilmCribFee) <= 25, "Total sell taxes cannot exceed 25%");
         _sellProductionFee = newSellProductionFee;
         _sellMarketingFee = newSellMarketingFee;
+        _sellFilmCribFee = newSellFilmCribFee;
         
         totalFees();
+
+        emit UpdateSellFees(newSellProductionFee, _sellProductionFee, newSellMarketingFee, _sellMarketingFee, newSellFilmCribFee, _sellFilmCribFee);
     }
 
     function updateMaxWallet(uint256 newMaxWallet) public onlyOwner {
         require(newMaxWallet >= 5000000, "Cannot lower max wallet below .5% of total supply");
         _maxWallet = newMaxWallet * (10**18);
+
+        emit UpdateMaxWallet(newMaxWallet, _maxWallet);
     }
 
     function updateMaxBuySell(uint256 newMaxBuy, uint256 newMaxSell) public onlyOwner {
@@ -1432,15 +1487,18 @@ contract Retrogression is Context, ERC20, Ownable {
         require(newMaxSell >= 2500000, "Cannot lower max sell below .25% of total supply");
         _maxBuy = newMaxBuy * (10**18);
         _maxSell = newMaxSell * (10**18);
+
+        emit UpdateMaxBuySell(newMaxBuy, _maxBuy, newMaxSell, _maxSell);
     }
 
     function totalFees() private {
-        totalBuyFees = _buyProductionFee.add(_buyMarketingFee);
-        totalSellFees = _sellProductionFee.add(_sellMarketingFee);
+        totalBuyFees = _buyProductionFee.add(_buyMarketingFee).add(_buyFilmCribFee);
+        totalSellFees = _sellProductionFee.add(_sellMarketingFee).add(_sellFilmCribFee);
     }
 
     function withdrawRemainingETH(address account, uint256 percent) public onlyOwner {
         require(percent > 0 && percent <= 100);
+        require(account != address(0), "ERC20: transfer to the zero address");
         uint256 percentage = percent.div(100);
         uint256 balance = address(this).balance.mul(percentage);
         
@@ -1449,7 +1507,7 @@ contract Retrogression is Context, ERC20, Ownable {
     }
 
     function withdrawRemainingToken(address account, uint256 amount) public onlyOwner {
-        require(amount <= balanceOf(address(this)), "Amount cannot exceed tokens in conract");
+        require(amount <= balanceOf(address(this)), "Amount cannot exceed tokens in contract");
         super._transfer(address(this), account, amount);
     }
 
